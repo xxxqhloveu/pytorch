ed7276634c045145918bba3554778a46267d8504
PaliC <sahancpal@gmail.com>
2023-05-24 19:13:36 -0700

    preinstall dependencies for torch inductor benchmarking in docker image
    ghstack-source-id: 2ec98f03fa73e6300d2304964827fc2850b1df84
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101306

--- .ci/docker/common/install_triton.sh
+++ .ci/docker/common/install_triton.sh
@@ -4,14 +4,6 @@ set -ex
 
 source "$(dirname "${BASH_SOURCE[0]}")/common_utils.sh"
 
-get_conda_version() {
-  as_jenkins conda list -n py_$ANACONDA_PYTHON_VERSION | grep -w $* | head -n 1 | awk '{print $2}'
-}
-
-conda_reinstall() {
-  as_jenkins conda install -q -n py_$ANACONDA_PYTHON_VERSION -y --force-reinstall $*
-}
-
 if [ -n "${ROCM_VERSION}" ]; then
   TRITON_REPO="https://github.com/ROCmSoftwarePlatform/triton"
   TRITON_TEXT_FILE="triton-rocm"
@@ -47,16 +39,16 @@ else
   pip_install "git+${TRITON_REPO}@${TRITON_PINNED_COMMIT}#subdirectory=python"
 fi
 
-if [ -n "${CONDA_CMAKE}" ]; then
-  # TODO: This is to make sure that the same cmake and numpy version from install conda
-  # script is used. Without this step, the newer cmake version (3.25.2) downloaded by
-  # triton build step via pip will fail to detect conda MKL. Once that issue is fixed,
-  # this can be removed.
-  #
-  # The correct numpy version also needs to be set here because conda claims that it
-  # causes inconsistent environment.  Without this, conda will attempt to install the
-  # latest numpy version, which fails ASAN tests with the following import error: Numba
-  # needs NumPy 1.20 or less.
-  conda_reinstall cmake="${CMAKE_VERSION}"
-  conda_reinstall numpy="${NUMPY_VERSION}"
-fi
+# if [ -n "${CONDA_CMAKE}" ]; then
+#   # TODO: This is to make sure that the same cmake and numpy version from install conda
+#   # script is used. Without this step, the newer cmake version (3.25.2) downloaded by
+#   # triton build step via pip will fail to detect conda MKL. Once that issue is fixed,
+#   # this can be removed.
+#   #
+#   # The correct numpy version also needs to be set here because conda claims that it
+#   # causes inconsistent environment.  Without this, conda will attempt to install the
+#   # latest numpy version, which fails ASAN tests with the following import error: Numba
+#   # needs NumPy 1.20 or less.
+#   conda_reinstall cmake="${CMAKE_VERSION}"
+#   conda_reinstall numpy="${NUMPY_VERSION}"
+# fi
